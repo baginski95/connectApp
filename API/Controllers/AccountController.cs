c966f1f2b2f3934fa87b08a7ba293e05d7c4c156
Filip <bagiczny3@gmail.com>
2021-04-07 01:12:24 +0200

    Added Dtos for Register and login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,13 +21,13 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if(await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
 
             using var hmac = new HMACSHA512();
 
             var user =  new AppUser
             {
-                UserName = registerDto.Username.ToLower(),
+                UserName = registerDto.UserName.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
@@ -39,7 +39,24 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<Actio
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+
+            if (user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i< computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());

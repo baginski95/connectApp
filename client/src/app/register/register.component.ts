799e49a9a99971b63bf833ac6c6418c387950080
Filip <bagiczny3@gmail.com>
2021-04-19 23:32:31 +0200

    Added bridge between home and register components

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+@Input() usersFromHomeComponent: any;
+@Output() cancelRegister = new EventEmitter();
 model: any = {};
 
   constructor() { }
@@ -18,6 +20,6 @@ model: any = {};
   }
 
   cancel(){
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 }

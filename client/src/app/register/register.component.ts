0210f4ded6005b683a44d356d632465672690ab1
Filip <bagiczny3@gmail.com>
2021-04-24 23:57:33 +0200

    Added guard for routing and started creating routes

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit , Input, EventEmitter, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { error } from 'selenium-webdriver';
 import { AccountService } from '../_services/account.service';
 
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 @Output() cancelRegister = new EventEmitter();
 model: any = {};
 
-  constructor(private AccountService: AccountService) { }
+  constructor(private AccountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +24,7 @@ model: any = {};
       this.cancel()
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 

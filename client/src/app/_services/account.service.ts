c4b43020922a931833083628c3ddf6d009034e03
Filip <bagiczny3@gmail.com>
2021-04-26 13:22:52 +0200

    Implemented Middleware that prints errors from exception handler

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,7 +18,9 @@ export class AccountService {
   {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
+        
         const user = response;
+        console.log(user);
         if(user) {
           localStorage.setItem('user', JSON.stringify(user))
           this.currentUserSource.next(user);
